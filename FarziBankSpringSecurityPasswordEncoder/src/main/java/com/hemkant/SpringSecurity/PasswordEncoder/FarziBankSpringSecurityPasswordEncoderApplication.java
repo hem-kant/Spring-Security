69d5920ccb93ae4029851c1b859e22987b223cbe
Hem Kant <hemkant.india@gmail.com>
2022-10-22 18:14:46 +0530

    Spring Security - CORs and CSRF with FrontEnd UI code

---
+++ FarziBankSpringSecurityPasswordEncoder/src/main/java/com/hemkant/SpringSecurity/PasswordEncoder/FarziBankSpringSecurityPasswordEncoderApplication.java
@@ -0,0 +1,13 @@
+package com.hemkant.SpringSecurity.PasswordEncoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarziBankSpringSecurityPasswordEncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarziBankSpringSecurityPasswordEncoderApplication.class, args);
+	}
+
+}
